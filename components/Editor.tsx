8df3136b292ebfe6e18d1a76b16fba3004192f85
David Umoru <umorudavido@gmail.com>
2025-02-27 23:25:02 +0100

    Add autosave functionality to the editor

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,13 +1,37 @@
 "use client";
 
+import { updateEntry } from "@/utils/api";
 import { useState } from "react";
+import { useAutosave } from "react-autosave";
 
-const Editor = ({ entry }) => {
+interface Entry {
+  id: string;
+  content: string;
+}
+
+const Editor = ({ entry }: { entry: Entry }) => {
   const [value, setValue] = useState(entry.content);
+  const [isLoading, setIsLoading] = useState(false);
 
-  return <div className="w-full h-full">
-    <textarea className="w-full h-full p-8 text-xl outline-none" value={value} onChange={e => setValue(e.target.value)}></textarea>
-  </div>;
+  useAutosave({
+    data: value,
+    onSave: async (_value) => {
+      setIsLoading(true);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const updated = await updateEntry(entry.id, _value);
+      setIsLoading(false);
+    },
+  });
+  return (
+    <div className="w-full h-full">
+      {isLoading && <div>...loading</div>}
+      <textarea
+        className="w-full h-full p-8 text-xl outline-none"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      ></textarea>
+    </div>
+  );
 };
 
 export default Editor;
