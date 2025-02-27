212f0cd8733fe01435d53c6f585f5a37f65b178e
David Umoru <umorudavido@gmail.com>
2025-02-27 14:26:51 +0100

    Add new user creation logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  const href = userId ? "/journal" : "/new-user";
+
   return (
     <div className="w-screen h-screen bg-black text-white flex justify-center items-center">
       <div className="w-full max-w-[600px] mx-auto">
@@ -11,7 +16,7 @@ export default function Home() {
           life— all it takes is a little honesty.
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-lg">
               Get started
             </button>
