e88365d5a3e3bc2ac0cd1a7874100bc4bbb78b86
David Umoru <umorudavido@gmail.com>
2025-03-01 11:00:56 +0100

    Add analysis section to journal entry page

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -17,9 +17,35 @@ const getEntry = async (id: string) => {
 
 const EntryPage = async ({ params }: { params: { id: string } }) => {
   const entry = await getEntry(params.id);
+  const analysisData = [
+    { name: "Subject", value: "" },
+    { name: "Summary", value: "" },
+    { name: "Mood", value: "" },
+    { name: "Negative", value: "False" },
+  ];
   return (
-    <div className="w-full h-full">
-      <Editor entry={entry} />
+    <div className="w-full h-full grid grid-cols-3">
+      <div className="col-span-2">
+        {entry ? <Editor entry={entry} /> : <div>Entry not found</div>}
+      </div>
+      <div className="border-l border-black/10">
+        <div className="bg-blue-400 px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((item) => (
+              <li
+                key={item.name}
+                className="px-2 py-4 flex items-center justify-between border-b border-t border-black/10"
+              >
+                <span className="text-black font-semibold">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
